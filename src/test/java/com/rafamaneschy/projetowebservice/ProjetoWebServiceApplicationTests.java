495bb03b62283bfe2c5f9b5b3e15ab96048d08df
Rafael Maneschy <rafamaneschy@gmail.com>
2021-09-23 13:31:19 -0300

    Initial commit

---
+++ src/test/java/com/rafamaneschy/projetowebservice/ProjetoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafamaneschy.projetowebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
